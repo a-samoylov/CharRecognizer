b495512151e9b41210535627fc6ac1e42b0efd9f
Alexander Samoylov <a.samoylovua@gmail.com>
2019-01-27 02:19:55 +0200

    add configs to project

---
+++ CharRecognizer/Configs.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace CharRecognizer
+{
+    class Configs
+    {
+        private static Configs _instance;
+
+        private Configs() { }
+
+        public static Configs GetInstance()
+        {
+            return _instance ?? (_instance = new Configs());
+        }
+
+        public string GetPathToData()
+        {
+            return ConfigurationManager.AppSettings.Get("path_to_data");
+        }
+
+        public string GetPathToFileStorage()
+        {
+            return ConfigurationManager.AppSettings.Get("path_to_file_storage");
+        }
+    }
+}
